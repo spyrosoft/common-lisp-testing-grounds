4ce603b6513e569eddbf16a9b791b9aa1a7adc25
Bennett Roesch <meta@spyrosoft.com>
2015-05-20 11:37:57 -0700

    Three working iterate demonstration examples.

--- iterate.lisp
+++ iterate.lisp
@@ -1,14 +1,23 @@
 (ql:quickload :iterate)
 
-(iter (repeat 100)
-			(print 'yes))
+;; Iterate does not seem to work unless used in conjunction with a package
 
-(defvar example '(5 4 3 2 1))
+(defpackage :iterate-demonstration
+	(:use :common-lisp :iterate))
 
-(let ((item))
-	(iter (for item in example)
-				(print item)))
+(in-package :iterate-demonstration)
 
-(let ((counter 0))
-	 (iter (for counter from 1 to 10)
-				 (collect counter)))
+
+(defun repeat-example ()
+	(iter (repeat 4)
+				(print 'repeat-this)))
+
+(defun for-in-example ()
+	(let ((item nil) (example '(5 4 3 2 1)))
+		(iter (for item in example)
+					(print item))))
+
+(defun from-to-example ()
+	(let ((counter 0))
+		(iter (for counter from 1 to 10)
+					(collect counter))))
