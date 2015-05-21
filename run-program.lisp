e457b1e0e0a5779405aaee8c8aac3053c57b6565
Bennett Roesch <meta@spyrosoft.com>
2015-05-21 14:46:54 -0700

    Add working stream piping example to run-program.

--- run-program.lisp
+++ run-program.lisp
@@ -16,3 +16,24 @@ zebras"))
 									 :input input-stream
 									 :output output-stream)
       output-stream)))
+
+;; Run `ls' and pipe to `grep' - output only
+
+(let ((ls-process (run-program "/bin/ls" '()
+															 ;; Must run in background to work in tandem with the additional `grep' process - :wait nil
+															 :wait nil
+															 :output :stream)))
+	(unwind-protect
+			 (with-open-stream (ls-process-output (process-output ls-process))
+				 (let ((grep-process (run-program "/usr/bin/grep" '("a")
+																					:input ls-process-output
+																					:output :stream)))
+					 (when grep-process
+						 (unwind-protect
+									(with-open-stream (grep-process-output (process-output grep-process))
+										(loop
+											 :for line := (read-line grep-process-output nil nil)
+											 :while line
+											 :collect line))
+							 (process-close grep-process)))))
+		(when ls-process (process-close ls-process)))))
