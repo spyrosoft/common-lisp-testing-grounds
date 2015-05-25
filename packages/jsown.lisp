ac552e20115d6c35d68a98c49889cbb75afb9ea2
Bennett Roesch <meta@spyrosoft.com>
2015-05-24 20:49:24 -0700

    Add two more jsown examples: iterate and extend.

--- packages/jsown.lisp
+++ packages/jsown.lisp
@@ -1,6 +1,6 @@
 (ql:quickload :jsown)
 
-(defvar input-json "{\"integer\":100, \"string\":\"string\", \"double-float\":1.234567890123456789, \"boolean\": true, \"dimensional-array\": [1,2,3,[1,2,3]]}")
+(defvar input-json "{\"integer\":100, \"string\":\"string\", \"double-float\":1.234567890123456789, \"boolean\": false, \"dimensional-array\": [1,2,3,[1,2,3]]}")
 
 (defvar parsed-input-json (jsown:parse input-json))
 (print parsed-input-json)
@@ -17,7 +17,18 @@
 (print (jsown:parse input-json "integer" "boolean"))
 
 ;; Encode to string
+;; Note that boolean false is converted to '(), then back to []
 (jsown:to-json parsed-input-json)
+
+;; Iterate through the keys and values
+(jsown:do-json-keys (example-key example-value)
+		parsed-input-json
+	(print example-key)
+	(print (typep example-value 'integer)))
+
+;; Add additional items to an existing JSON object
+(jsown:extend-js parsed-input-json
+	("negative-integer" -200))
 
 ;; Create a new object from scratch
 (jsown:new-js
