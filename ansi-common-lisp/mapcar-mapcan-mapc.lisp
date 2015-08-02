276ee5c696651cc7d160a8fda3aef16580be7775
Bennett Roesch <meta@spyrosoft.com>
2015-08-02 13:19:13 -0700

    Keep it DRY. Add comment to mapcan regarding nconc.

--- ansi-common-lisp/mapcar-mapcan-mapc.lisp
+++ ansi-common-lisp/mapcar-mapcan-mapc.lisp
@@ -1,5 +1,12 @@
-(mapcar #'(lambda (n) (when (evenp n) (list n))) '(1 2 3 4 5 6 7 8 9))
+(defun when-even-make-list (number)
+  (when (evenp number) (list number)))
 
-(mapcan #'(lambda (n) (when (evenp n) (list n))) '(1 2 3 4 5 6 7 8 9))
+(defvar example-list '(1 2 3 4 5 6 7 8 9))
 
-(mapc #'(lambda (n) (when (evenp n) (list n))) '(1 2 3 4 5 6 7 8 9))
+
+(mapcar #'when-even-make-list example-list)
+
+;; Caution Destructive - uses `nconc'
+(mapcan #'when-even-make-list example-list)
+
+(mapc #'when-even-make-list example-list)
