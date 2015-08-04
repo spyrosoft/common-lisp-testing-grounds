ffbe8283c9d92b34e9fc2a09b1aa364f8557a910
Bennett Roesch <meta@spyrosoft.com>
2015-08-03 19:22:26 -0700

    Add docs for creating & using doubly linked lists.

---
+++ data-structures/doubly-linked-list.lisp
@@ -0,0 +1,32 @@
+;; Origin ANSI Common Lisp by Paul Graham
+(defstruct (dl (:print-function print-dl))
+  prev data next)
+
+(defun print-dl (dl stream depth)
+  (declare (ignore depth))
+  (format stream "#<DL ~A>" (dl->list dl)))
+
+(defun dl->list (lst)
+  (if (dl-p lst)
+      (cons (dl-data lst) (dl->list (dl-next lst)))
+      lst))
+
+(defun dl-insert (x lst)
+  (let ((elt (make-dl :data x :next lst)))
+    (when (dl-p lst)
+      (if (dl-prev lst)
+          (setf (dl-next (dl-prev lst)) elt
+                (dl-prev elt) (dl-prev lst)))
+      (setf (dl-prev lst) elt))
+    elt))
+
+(defun dl-list (&rest args)
+  (reduce #'dl-insert args
+          :from-end t :initial-value nil))
+
+(defun dl-remove (lst)
+  (if (dl-prev lst)
+      (setf (dl-next (dl-prev lst)) (dl-next lst)))
+  (if (dl-next lst)
+      (setf (dl-prev (dl-next lst)) (dl-prev lst)))
+  (dl-next lst))
