263943f8f224f4c471c51583c0ead5b62a68ebf5
Bennett Roesch <meta@spyrosoft.com>
2015-08-09 16:25:27 -0700

    Add solutions to Chapter 4 Problem 2 (a) & (b) - using reduce to recreate copy-list and reverse.

--- exercises-from-references/ansi-common-lisp/lists-exercises.lisp
+++ exercises-from-references/ansi-common-lisp/lists-exercises.lisp
@@ -140,3 +140,17 @@
 ;; #2A((C A) (D B))
 (quarter-turn #2A((a b) (c d)))
 ;; #2A((D A B) (D E F) (G H I))
+
+
+;; 2. Use the reduce function to achieve the following:
+;; (a) copy-list
+
+(defun reduce-copy-list (list-to-copy)
+  (reduce #'cons list-to-copy :from-end t :initial-value '()))
+
+;; (b) reverse (lists only)
+
+(defun reduce-reverse (list-to-reverse)
+  (reduce #'(lambda (item1 item2) (cons item2 item1)) list-to-reverse :initial-value '()))
+
+
