1c72a6a7ce0dfa1c250c9e4fa0fb1064af816663
Bennett Roesch <meta@spyrosoft.com>
2015-08-09 15:54:01 -0700

    Add solution for ANSI Common Lisp Chapter 4 Problem 1 - Turn a square 2d array 90 degrees clockwise.

--- exercises-from-references/ansi-common-lisp/lists-exercises.lisp
+++ exercises-from-references/ansi-common-lisp/lists-exercises.lisp
@@ -112,3 +112,22 @@
   (mapcar #'(lambda (new-path)
               (cons new-path path))
           (cdr (assoc node network))))
+
+
+
+;; Chapter 4 - Specialized Data Structures
+;; 1. Define a function to take a square array (an array whose dimensions are (n n)) and rotate it 90° clockwise:
+
+(defun quarter-turn (array-to-turn)
+  (let* ((square-dimensions (first (array-dimensions array-to-turn)))
+         (turned-array (make-array (list square-dimensions square-dimensions))))
+    (dotimes (row square-dimensions)
+      (dotimes (column square-dimensions)
+        (setf (aref turned-array row column)
+              (aref array-to-turn (- square-dimensions column 1) row))))
+    turned-array))
+
+(quarter-turn #2A((a b) (c d)))
+;; #2A((C A) (D B))
+(quarter-turn #2A((a b) (c d)))
+;; #2A((D A B) (D E F) (G H I))
