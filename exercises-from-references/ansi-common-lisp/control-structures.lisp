b647663f6020612f1a4d1bf05e50421981854b95
Bennett Roesch <meta@spyrosoft.com>
2015-08-10 17:35:29 -0700

    Correct the lambda versions of let and let*. Add solution to problem 6 for implementing join with lists.

--- exercises-from-references/ansi-common-lisp/control-structures.lisp
+++ exercises-from-references/ansi-common-lisp/control-structures.lisp
@@ -1,12 +1,45 @@
 ;; Chapter 5 - Control Structures
 ;; 1. Translate the following expressions into equivalent expressions that don't use let or let*, and don't cause the same expression to be evaluated twice.
-;; (a) (let ((x (car y)))
-;;       (cons x x))
+;; (a)
 
-((lambda (x) (cons x x) (car y)))
+(defvar y '(y))
+(let ((x (car y)))
+  (cons x x))
+
+((lambda (x)
+   (cons x x))
+ (car y))
 
 ;; (b) (let* ((w (car x))
-;;        (y (+ v z)))
+;;        (y (+ w z)))
 ;;       (cons w y))
 
-((lambda ()))
+(defvar x '(1))
+(defvar z '2)
+;; Equivalent to:
+(let ((w (car x)))
+  (let ((y (+ w z)))
+    (cons w y)))
+
+;; Equivalent to:
+((lambda (w)
+   ((lambda (y)
+      (cons w y))
+    (+ w z)))
+ (car x))
+
+
+;; 6. Define iterative and recursive versions of a function that takes an object and a list, and returns a new list in which the object appears between each pair of elements in the original list.
+
+(defun intersperse (join-character list-to-join)
+  (let ((joined-list '()))
+    (dolist (item list-to-join)
+      (push item joined-list)
+      (push '- joined-list))
+    (pop joined-list)
+    (reverse joined-list)))
+
+(intersperse '- '(a b c d))
+;; (A - B - C - D)
+
+
