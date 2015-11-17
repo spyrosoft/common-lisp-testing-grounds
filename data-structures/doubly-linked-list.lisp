98b9252882197da8098e26ee410dae94625a6cc2
Bennett Roesch <meta@spyrosoft.com>
2015-11-16 22:49:39 -0800

    Complete marginally functional doubly linked list.

--- data-structures/doubly-linked-list.lisp
+++ data-structures/doubly-linked-list.lisp
@@ -1,3 +1,27 @@
+;; Conceptual origin: http://stackoverflow.com/questions/5764394/data-structures-in-lisp/5764901?s=3%7C0.6456#5764901
+(defpackage #:doubly-linked-list
+  (:use #:cl))
+
+(in-package #:doubly-linked-list)
+
+(setf *print-circle* t)
+
+(defun new (list-item-value)
+  (cons list-item-value (cons nil nil)))
+
+(defun convert-singly-linked-list (singly-linked-list)
+  (let ((dl-list-head (new-dl-item)))
+    (dolist (item singly-linked-list)
+      (if (car dl-list-head)
+        (setf (car dl-list-head) item)))))
+
+(defun insert (list-item-value doubly-linked-list)
+  (let ((new-list (new list-item-value)))
+    (setf (cddr new-list) doubly-linked-list)
+    (setf (cadr doubly-linked-list) new-list)
+    new-list))
+
+
 ;; Origin ANSI Common Lisp by Paul Graham
 (defstruct (dl (:print-function print-dl))
   prev data next)
