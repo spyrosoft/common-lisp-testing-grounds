98f6169edda6aaf1dcb4cf945c78d09d0172741f
Bennett Roesch <meta@spyrosoft.com>
2015-11-23 12:50:53 -0800

    Optimization: add macro ,@ to avoid consing &rest.

--- exercises-from-references/ansi-common-lisp/optimization.lisp
+++ exercises-from-references/ansi-common-lisp/optimization.lisp
@@ -19,8 +19,10 @@
 ;; The compiler treats inlining declamations as fact - it will always do so when requested
 ;; The declaim statement must come before any code where the function is used
 ;; Inlining functions shaves quite a bit of overhead off when a function is called many times, for example in a loop. But by doing so, it is less space efficient - that said, this is the 21st century, so space pretty much doesn't matter anymore
+
 (defvar *max-iterations*)
 (setq *max-iterations* 100000000)
+
 (defun time-no-inlining (dummy-input)
   (* dummy-input dummy-input))
 
@@ -38,3 +40,31 @@
           ((= i *max-iterations*))
         (time-inlining i)))
 ;; 4.289 seconds
+
+
+
+;; Use macros to avoid consing &rest
+;; Note that on most implementations, inlining the function effectively accomplishes the same goal
+(defmacro recreate-list-macro (&rest list-items)
+  `(list ,@list-items))
+
+(defun time-recreate-list-macro ()
+  (do ((i 0 (1+ i)))
+      ((= i *max-iterations*))
+    (recreate-list-macro 1 2 3 4 5 6 7 8)))
+
+(time (time-recreate-list-macro))
+;; 0.465 seconds
+
+;; This function is useless and counter-productive
+;; But you get the idea
+(defun recreate-list-function (&rest list-items)
+  (apply #'list list-items))
+
+(defun time-recreate-list-function ()
+  (do ((i 0 (1+ i)))
+      ((= i *max-iterations*))
+    (recreate-list-function 1 2 3 4 5 6 7 8)))
+
+(time (time-recreate-list-function))
+;; 9.909 seconds
